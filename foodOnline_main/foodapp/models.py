96fc6eab1d152185dd2080045f675134d07a83de
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-10-05 13:19:23 +0300

    addingUserModel

--- foodOnline_main/foodapp/models.py
+++ foodOnline_main/foodapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, first_name,last_name, username, email, password=None):
+        if not email:
+            raise ValueError('Please provide an email address')
+        if not username:
+            raise ValueError('Please provide a username')
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.set_password(password)
+        user.save(using = self.db)
+        
+        return user
+    
+    def create_superuser(self, first_name,last_name, username, email, password=None):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using = self.db)
+        return user
+    
+    
