2a720ade5f646ee7c0fa6077fdcf8ef43040f0b0
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-10-05 13:58:07 +0300

    addingManagUserModel

--- foodOnline_main/foodapp/models.py
+++ foodOnline_main/foodapp/models.py
@@ -33,4 +33,43 @@ class UserManager(BaseUserManager):
         user.save(using = self.db)
         return user
     
+
+class User(AbstractBaseUser):
+    RESTURANT = 1
+    CUSTOMER = 2
+    ROLE_CHOICE = (
+        (RESTURANT,'RESTURANT'),
+        (CUSTOMER,'CUSTOMER'),
+    )
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    username = models.CharField(max_length = 50, unique = True)
+    email = models.EmailField(max_length =100, blank=True)
+    phone_number = models.CharField(max_length =12, blank=True)
+    role = models.PositiveSmallIntegerField(choices = ROLE_CHOICE, blank=True,null=True)
+    
+    #required fields
+    
+    date_joined =models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    modifired_date = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)   
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)   
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS= ['username','first_name','last_name']
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
     
